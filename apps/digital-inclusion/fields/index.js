1dda47ce74cfdf72cb15585c022aeb280e142511
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2018-01-08 13:00:16 +0000

    Add pages & fields for /research-date, /category, /access-needs

--- apps/digital-inclusion/fields/index.js
+++ apps/digital-inclusion/fields/index.js
@@ -1,7 +1,34 @@
 'use strict';
+const dateComponent = require('hof-component-date');
 
 module.exports = {
-  name: {
-    validate: 'required'
+  'research-date': dateComponent('research-date'),
+  'category': {
+    mixin: 'radio-group',
+    validate: 'required',
+    legend: {
+      className: 'visuallyhidden'
+    },
+    options: [
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9'
+    ]
+  },
+  'access-needs': {
+    mixin: 'radio-group',
+    legend: {
+      className: 'visuallyhidden'
+    },
+    options: [
+      'yes',
+      'no'
+    ]
   }
 };
