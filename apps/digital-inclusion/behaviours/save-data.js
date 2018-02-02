27f1574c43c2eee548aa35421287ef8ce32886dd
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2018-02-02 11:47:00 +0000

    Amend Save Data behaviour to sql
    Reformat the code so the data to the db and then super is called

--- apps/digital-inclusion/behaviours/save-data.js
+++ apps/digital-inclusion/behaviours/save-data.js
@@ -12,46 +12,44 @@ const pool = mysql.createPool({
 
 module.exports = superclass => class extends superclass {
   saveValues(req, res, next) {
-    super.saveValues(req, res, cb => {
-      const project = req.sessionModel.get('project');
-      const researchDate = req.sessionModel.get('research-date');
-      const category = req.sessionModel.get('category');
-      const accessNeeds = req.sessionModel.get('access-needs');
+    const project = req.sessionModel.get('project');
+    const researchDate = req.sessionModel.get('research-date');
+    const category = req.sessionModel.get('category');
+    const accessNeeds = req.sessionModel.get('access-needs');
 
-      // build sql statement to deal with empty researchDate or/and accessNeeds,
-      // this will mean a null will go into the db
-      // bug: mysql needs insert data to be wrapped in quotations '' even though it is a string
-      let sql = `INSERT INTO research_data SET project='${project}', category='${category}'`;
-      if (researchDate) {
-        sql += `, research_date='${researchDate}'`;
-      }
-      if (accessNeeds) {
-       sql += `, access_needs='${accessNeeds}'`;
-      }
+    // build sql statement to deal with empty researchDate or/and accessNeeds,
+    // this will mean a null will go into the db
+    // bug: mysql needs insert data to be wrapped in quotations '' even though it is a string
+    let sql = `INSERT INTO research_data SET project='${project}', category='${category}'`;
+    if (researchDate) {
+      sql += `, research_date='${researchDate}'`;
+    }
+    if (accessNeeds) {
+     sql += `, access_needs='${accessNeeds}'`;
+    }
 
-      pool.getConnection((err, connection) => {
-      if (err) {
-        // eslint-disable-next-line no-console
-        console.error('error connecting: ' + err.stack);
-        next(err);
-      }
+    pool.getConnection((err, connection) => {
+    if (err) {
       // eslint-disable-next-line no-console
-      console.info('reconnected to the db!');
+      console.error('error connecting: ' + err.stack);
+      next(err);
+    }
+    // eslint-disable-next-line no-console
+    console.info('reconnected to the db!');
 
-        connection.query(sql, (error, result) => {
-          // when done release the connection to be reused
-          connection.release();
-          if (error) {
-            // eslint-disable-next-line no-console
-            console.error('error connecting: ' + err.stack);
-            next(error);
-          }
+      connection.query(sql, (error, result) => {
+        // when done release the connection to be reused
+        connection.release();
+        if (error) {
           // eslint-disable-next-line no-console
-          console.info(`Number of records inserted: ${result.affectedRows}`);
-        });
+          console.error('error connecting: ' + err.stack);
+          next(error);
+        }
+        // eslint-disable-next-line no-console
+        console.info(`Number of records inserted: ${result.affectedRows}`);
       });
-
-      next(cb);
     });
+
+    super.saveValues(req, res, next);
   }
 };
