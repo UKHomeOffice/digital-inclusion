bc0a71264ab1862c4905cbfd718c1d4f5323b9d2
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2018-03-22 14:11:44 +0000

    Remove connection to mysql at start of app
    This is not needed because sql pools are being used

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
 'use strict';
 
 require('./index').start();
-
-const mysql = require('mysql');
-const config = require('./config');
-const connection = mysql.createConnection({
-  host: config.mysql.host,
-  user: config.mysql.user,
-  password: config.mysql.password,
-  database: config.mysql.db
-});
-
-connection.connect((err) => {
-  if (err) {
-    // eslint-disable-next-line no-console
-    console.error('error connecting: ' + err.stack);
-    return err;
-  }
-  // eslint-disable-next-line no-console
-  console.log('Db connected as id ' + connection.threadId);
-});
