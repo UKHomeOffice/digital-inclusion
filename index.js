e5526ea0b4207d91301d9c98dd49325d9407f4b5
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2018-05-04 15:15:15 +0100

    Move hof.settings.json into index.js
    It makes it easier and quicker to read

--- index.js
+++ index.js
@@ -2,7 +2,19 @@
 
 const hof = require('hof');
 
-const settings = require('./hof.settings');
+const settings = {
+  theme: 'govuk',
+  routes: [
+    './apps/digital-inclusion'
+  ],
+  session: {
+    name: 'digital-inclusion.hof.sid'
+  },
+  // allows you to have to load browserify file in your views
+  csp: {
+    styleSrc: ["'unsafe-inline'"]
+  }
+};
 
 settings.routes = settings.routes.map(route => require(route));
 settings.root = __dirname;
